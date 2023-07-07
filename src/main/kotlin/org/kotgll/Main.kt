335d9c26918600c763eda6c0772a58478a2be9a4
cyb3r-b4stard <hollowcoder@yandex.ru>
2023-07-07 15:11:20 +0300

    removed redundant options from Main

--- src/main/kotlin/org/kotgll/Main.kt
+++ src/main/kotlin/org/kotgll/Main.kt
@@ -7,54 +7,52 @@ import org.kotgll.graph.readGraphFromCSV
 import org.kotgll.rsm.grammar.readRSMFromTXT
 import java.io.File
 
-enum class InputMode
-{
+enum class InputMode {
     STRING,
     GRAPH,
 }
 
-fun main(args : Array<String>)
-{
-  val parser = ArgParser("kotgll")
+fun main(args: Array<String>) {
+    val parser = ArgParser("kotgll")
 
-  val inputMode by
-      parser
-          .option(ArgType.Choice<InputMode>(), fullName = "input", description = "Input format")
-          .required()
+    val inputMode by
+    parser
+        .option(ArgType.Choice<InputMode>(), fullName = "input", description = "Input format")
+        .required()
 
-  val pathToInput by
-      parser
-          .option(ArgType.String, fullName = "inputPath", description = "Path to input txt file")
-          .required()
+    val pathToInput by
+    parser
+        .option(ArgType.String, fullName = "inputPath", description = "Path to input txt file")
+        .required()
 
-  val pathToGrammar by
-      parser
-          .option(ArgType.String, fullName = "grammarPath", description = "Path to grammar txt file")
-          .required()
+    val pathToGrammar by
+    parser
+        .option(ArgType.String, fullName = "grammarPath", description = "Path to grammar txt file")
+        .required()
 
-  val pathToOutput by
-      parser
-          .option(ArgType.String, fullName = "outputPath", description = "Path to output txt file")
-          .required()
+    val pathToOutput by
+    parser
+        .option(ArgType.String, fullName = "outputPath", description = "Path to output txt file")
+        .required()
 
-  parser.parse(args)
+    parser.parse(args)
 
-  if (inputMode == InputMode.STRING) {
-      val input   = File(pathToInput).readText()
-      val grammar = readRSMFromTXT(pathToGrammar)
-      val result  = org.kotgll.rsm.stringinput.withsppf.GLL(grammar, input).parse()
+    if (inputMode == InputMode.STRING) {
+        val input = File(pathToInput).readText()
+        val grammar = readRSMFromTXT(pathToGrammar)
+        val result = org.kotgll.rsm.stringinput.withsppf.GLL(grammar, input).parse()
 
-      File(pathToOutput).printWriter().use { out -> out.println(result != null) }
+        File(pathToOutput).printWriter().use { out -> out.println(result != null) }
 
-  } else if (inputMode == InputMode.GRAPH) {
-      val graph = readGraphFromCSV(pathToInput)
-      val grammar = readRSMFromTXT(pathToGrammar)
-      val result = org.kotgll.rsm.graphinput.withsppf.GLL(grammar, graph).parse()
+    } else if (inputMode == InputMode.GRAPH) {
+        val graph = readGraphFromCSV(pathToInput)
+        val grammar = readRSMFromTXT(pathToGrammar)
+        val result = org.kotgll.rsm.graphinput.withsppf.GLL(grammar, graph).parse()
 
-      File(pathToOutput).printWriter().use {
-          out -> result.keys.forEach {
-              tail -> result[tail]!!.keys.forEach { head -> out.println("$tail $head") }
-          }
-      }
-  }
+        File(pathToOutput).printWriter().use { out ->
+            result.keys.forEach { tail ->
+                result[tail]!!.keys.forEach { head -> out.println("$tail $head") }
+            }
+        }
+    }
 }
